9dc55582e661b8fbefe310fb6d8ee41951d68761
Nicolas Sorin <nicolas.sorin1996@gmail.com>
2019-09-18 18:13:22 +0200

    Changed config for dev

--- src/axios-config.js
+++ src/axios-config.js
@@ -1,5 +1,5 @@
 const config = {
-  baseURL: process.env.NODE_ENV === 'production' ? 'http://debian/api' : 'http://localhost',
+  baseURL: process.env.NODE_ENV === 'production' ? 'http://debian/api' : 'http://localhost:8080',
   url: '/',
   headers: {
     'Accept': 'application/json',
